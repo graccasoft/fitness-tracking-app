1ebb7355006265b013a8713614abdd9d6a657ac8
graccasoft <graccasoft263@gmail.com>
2025-04-12 21:59:54 +0200

    AuthService login and register stubs

--- fitness-tracking-app/Services/AuthService.cs
+++ fitness-tracking-app/Services/AuthService.cs
@@ -3,8 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using fitness_tracking_app.Models;
+using fitness_tracking_app.Repositories;
 
 namespace fitness_tracking_app.Services {
     internal class AuthService {
+        private readonly BaseRepository<User> repository;
+
+        AuthService() {
+            repository = new BaseRepository<User>();
+        }
+
+        public User authenticate(User user) {
+
+            return null;
+        }
+
+        public User register(User user) {
+            if (!Validator.isValidUsername(user.Username)) {
+                Notifications.warn("Username is not valid");
+                return null;
+            }
+            if (!Validator.isValidPassword(user.Password)) {
+                Notifications.warn("Password is not valid");
+                return null;
+            }
+
+            repository.save(user);
+
+            return user;
+        }
+
     }
 }
