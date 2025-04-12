10de192e0a324a6b251b3ebd4d15d9a74e28815e
graccasoft <graccasoft263@gmail.com>
2025-04-12 20:03:01 +0200

    Enhance ActivityMetric class with new properties and methods

--- fitness-tracking-app/Models/ActivityMetric.cs
+++ fitness-tracking-app/Models/ActivityMetric.cs
@@ -5,6 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace fitness_tracking_app.Models {
-    internal class ActivityMetric {
+    internal class ActivityMetric: BaseEntity {
+        public override string GetTableName() {
+            return "activity_metrics";
+        }
+
+        public string Activity { get; set; }
+        public string Metric1 { get; set; }
+        public string Metric1Formulae { get; set; }
+        public string Metric2 { get; set; }
+        public string Metric2Formulae { get; set; }
+        public string Metric3 { get; set; }
+        public string Metric3Formulae { get; set; }
+
     }
 }
