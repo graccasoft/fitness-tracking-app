4ad52432897abf8123ed707a034ac59e7483f46f
graccasoft <graccasoft263@gmail.com>
2025-04-12 20:08:34 +0200

    Enhance UserActivity class with inheritance and new properties

--- fitness-tracking-app/Models/UserActivity.cs
+++ fitness-tracking-app/Models/UserActivity.cs
@@ -5,6 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace fitness_tracking_app.Models {
-    internal class UserActivity {
+    internal class UserActivity : BaseEntity {
+        public override string GetTableName() {
+            return "user_activity";
+        }
+        public int UserId { get; set; }
+        public int MetricId { get; set; }
+        public double Value { get; set; }
+        public string Metric { get; set; }
+
     }
 }
