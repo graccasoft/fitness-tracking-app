5475e207fc901beacc2b4970d8e88cfecd7795fd
graccasoft <graccasoft263@gmail.com>
2025-04-12 19:21:55 +0200

    Add Gender enum and enhance User class with new properties

--- fitness-tracking-app/Models/User.cs
+++ fitness-tracking-app/Models/User.cs
@@ -5,6 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace fitness_tracking_app.Models {
-    internal class User {
+    internal class User : BaseEntity {
+
+        public override String GetTableName() {
+            return "users";
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public Gender gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
     }
 }
