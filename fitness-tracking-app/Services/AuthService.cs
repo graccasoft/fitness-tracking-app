5323ad251d34f5dff23b689e75ffbf83727dd3cd
graccasoft <graccasoft263@gmail.com>
2025-04-12 21:15:31 +0200

    Bootstrap service classes for the business logic.

---
+++ fitness-tracking-app/Services/AuthService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_tracking_app.Services {
+    internal class AuthService {
+    }
+}
