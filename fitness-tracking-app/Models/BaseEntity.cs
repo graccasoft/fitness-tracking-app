3ee6201902edeb97b4f3be0e799f84cd11aedd6c
graccasoft <graccasoft263@gmail.com>
2025-04-12 19:21:28 +0200

    Make BaseEntity abstract, add properties and methods

--- fitness-tracking-app/Models/BaseEntity.cs
+++ fitness-tracking-app/Models/BaseEntity.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace fitness_tracking_app.Models {
-    internal class BaseEntity {
+    internal abstract class BaseEntity {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public abstract string GetTableName();
+
+        public Dictionary<string, object> GetFieldValues() {
+            Dictionary<string, object> fieldValues = new Dictionary<string, object>();
+            Type type = this.GetType();
+
+            foreach (PropertyInfo property in type.GetProperties()) {
+                fieldValues[property.Name] = property.GetValue(this);
+            }
+
+            return fieldValues;
+        }
     }
 }
