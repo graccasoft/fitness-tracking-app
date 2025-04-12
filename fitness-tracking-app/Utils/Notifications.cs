93ce6a960c1d47938345b3a28e447ac72f69219d
graccasoft <graccasoft263@gmail.com>
2025-04-12 21:58:56 +0200

    Add Notifications and Validator util Classes

---
+++ fitness-tracking-app/Utils/Notifications.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_tracking_app.Services {
+    internal class Notifications {
+        public static void info(String message) {
+            MessageBox.Show(message, "Fitness Tracking App", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void warn(String message) {
+            MessageBox.Show(message, "Fitness Tracking App", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+    }
+}
---
+++ fitness-tracking-app/Utils/Validator.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace fitness_tracking_app.Services {
+    internal class Validator {
+
+        public static Boolean isValidPassword(String password) {
+            
+            if (password.Trim().Length < 12) {
+                return false;
+            }
+
+            Boolean hasNumbers = false;
+            Boolean hasCaps = false;
+            Boolean hasSmall = false;
+
+            for (int x = 0; x < password.Length; x++) {
+                if (password[x] >= 'a' && password[x] <= 'z') {
+                    hasSmall = true;
+                }
+                if (password[x] >= 'A' && password[x] <= 'Z') {
+                    hasCaps = true;
+                }
+                if (password[x] >= '0' && password[x] <= '9') {
+                    hasNumbers = true;
+                }
+            }
+            return hasNumbers && hasCaps && hasSmall;
+        }
+
+        public static Boolean isValidUsername(String username) {
+            if (username.Trim().Length < 6) {
+                return false;
+            }
+            //only letters and underscores
+
+            return Regex.IsMatch(username, @"^[a-zA-Z_]+$");
+        }
+
+        public static bool ctlVerifyLength(Control ctl, int minLen, int maxLen, String error = "Error from ctlVerifyLength") {
+            if ((ctl.Text.Trim().Length >= minLen && ctl.Text.Trim().Length <= maxLen))
+                return true;
+            MessageBox.Show(error);
+            ctl.Focus();
+            return false;
+        }
+
+
+    }
+}
