e2d28c1111f5999477eabd3d28fd05fd0bec31fb
graccasoft <graccasoft263@gmail.com>
2025-04-12 20:11:23 +0200

    Enhance UserGoal class with inheritance and new properties

--- fitness-tracking-app/Models/UserGoal.cs
+++ fitness-tracking-app/Models/UserGoal.cs
@@ -5,6 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace fitness_tracking_app.Models {
-    internal class UserGoal {
+    internal class UserGoal : BaseEntity {
+
+        public override string GetTableName() {
+            return "user_goal";
+        }
+        public int UserId { get; set; }
+        public double goal { get; set; }
+
     }
 }
