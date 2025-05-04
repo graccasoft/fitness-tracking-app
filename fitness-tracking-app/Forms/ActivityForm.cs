d0cc4631fb89fecc5338fa760fc0b8c4b5571f47
graccasoft <graccasoft263@gmail.com>
2025-05-04 10:26:11 +0200

    Activity form impl

--- fitness-tracking-app/Forms/ActivityForm.cs
+++ fitness-tracking-app/Forms/ActivityForm.cs
@@ -7,11 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using fitness_tracking_app.Models;
+using fitness_tracking_app.Services;
 
 namespace fitness_tracking_app.Forms {
     public partial class ActivityForm : Form {
+
+        private readonly ActivityService activityService;
+        private List<ActivityMetric> activityMetrics;
+
         public ActivityForm() {
+
             InitializeComponent();
+            activityService = new ActivityService();
+            this.activityMetrics = activityService.GetActivityMetricList();
         }
 
         private void label1_Click(object sender, EventArgs e) {
@@ -19,7 +28,11 @@ namespace fitness_tracking_app.Forms {
         }
 
         private void ActivityForm_Load(object sender, EventArgs e) {
-           
+            //load activities in combobox
+            foreach (var activity in activityMetrics) {
+                cmbActivities.Items.Add(activity.Activity);
+            }
+
         }
     }
 }
--- fitness-tracking-app/Services/ActivityService.cs
+++ fitness-tracking-app/Services/ActivityService.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using fitness_tracking_app.Models;
+using fitness_tracking_app.Repositories;
 
 namespace fitness_tracking_app.Services {
+
+    
     internal class ActivityService {
+        private readonly BaseRepository<ActivityMetric> metricRepository;
+
+        public ActivityService() {
+            metricRepository = new BaseRepository<ActivityMetric>();
+        }
+
+        public List<ActivityMetric> GetActivityMetricList() {
+            return metricRepository.getAll() ;
+        }
     }
 }
